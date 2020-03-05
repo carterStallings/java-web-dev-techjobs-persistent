fbd36f96ad9f3d011a31d737e5561433c249f678
CS051376 <carterstallings1848@gmail.com>
2020-03-04 19:12:52 -0600

    commiting sql query fixes for Tom

--- queries.sql
+++ queries.sql
@@ -1,17 +1,12 @@
 ## Part 1: Test it with SQL
-int(id)
-VARCHAR(employer)
-VARCHAR(name)
-VARCHAR(skills)
-
-int(id)
+int(id), primary key
 VARCHAR(name)
 int(employer_id)
 
 ## Part 2: Test it with SQL
 SELECT name
 FROM employer
-WHERE (location = "St. Louis City");
+WHERE location = "St. Louis City";
 
 
 ## Part 3: Test it with SQL
